d2145c4c996ba76c61078505dc66e63fef3beb50
mtai0 <150003923+mtai0@users.noreply.github.com>
2024-04-29 13:05:41 -0700

    Fixed ID field in database

--- src/main/resources/db-setup.sql
+++ src/main/resources/db-setup.sql
@@ -4,19 +4,19 @@
 PRAGMA foreign_keys = 1;
 
 create table users(
-	id serial primary key,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	username varchar(20) unique,
 	password varchar(20)
 );
 
 create table planets(
-	id serial primary key,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name varchar(20),
 	ownerId int references users(id)
 );
 
 create table moons(
-	id serial primary key,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name varchar(20),
 	myPlanetId int references planets(id)
 );
