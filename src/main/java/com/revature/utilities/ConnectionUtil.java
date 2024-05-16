3a17a8570c11a87758822c40543e9f2662d014aa
mtai0 <150003923+mtai0@users.noreply.github.com>
2024-05-16 10:30:20 -0700

    some setup for testing

--- src/main/java/com/revature/utilities/ConnectionUtil.java
+++ src/main/java/com/revature/utilities/ConnectionUtil.java
@@ -1,13 +1,31 @@
 package com.revature.utilities;
 
+import com.revature.MainDriver;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
     
     public static Connection createConnection() throws SQLException{
-        return DriverManager.getConnection("jdbc:sqlite:src/main/resources/planetarium.db");
+        InputStream props= MainDriver.class.getClassLoader().getResourceAsStream("database.properties");
+        Properties properties=new Properties();
+        try{
+            properties.load(props);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        boolean useTest = Boolean.parseBoolean(properties.getProperty("test-mode"));
+        if (useTest) {
+            return DriverManager.getConnection("jdbc:sqlite:src/main/resources/planetariumForTest.db");
+        }
+        else {
+            return DriverManager.getConnection("jdbc:sqlite:src/main/resources/planetarium.db");
+        }
     }
 
     public static void main(String[] args) {
