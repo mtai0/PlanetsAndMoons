9a9f47c13eadda1f79d42590323b9ff776fa566b
mtai0 <150003923+mtai0@users.noreply.github.com>
2024-05-18 13:42:08 -0700

    Finished unit tests for PlanetServiceTest.java

--- src/test/java/com/revature/units/PlanetServiceTest.java
+++ src/test/java/com/revature/units/PlanetServiceTest.java
@@ -12,6 +12,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.when;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -33,13 +36,46 @@ public class PlanetServiceTest {
         planetService = null;
     }
 
+    @Test
+    @DisplayName("PlanetService::getAllPlanets - Success")
+    @Order(0)
+    public void getAllPlanetsSingle() {
+        ArrayList<Planet> toReturn = new ArrayList<Planet>();
+        Planet p1 = new Planet();
+        p1.setName("Planet 1");
+        p1.setOwnerId(1);
+        p1.setId(1);
+        toReturn.add(p1);
+
+        Planet p2 = new Planet();
+        p2.setName("Planet 2");
+        p2.setOwnerId(1);
+        p2.setId(2);
+        toReturn.add(p2);
+
+        when(dao.getAllPlanets(1)).thenReturn(toReturn);
+
+        List<Planet> actual = planetService.getAllPlanets(1);
+        Assertions.assertEquals(2, actual.size());
+    }
+
+    @Test
+    @DisplayName("PlanetService::getAllPlanets - No Planets")
+    @Order(1)
+    public void getAllPlanetsNoPlanets() {
+        when(dao.getAllPlanets(1)).thenReturn(new ArrayList<Planet>());
+        List<Planet> actual = planetService.getAllPlanets(1);
+        Assertions.assertEquals(0, actual.size());
+    }
+
     @ParameterizedTest
     @DisplayName("PlanetService::getPlanetByName - Success")
+    @Order(2)
     @CsvSource({
             "1,Planet1",
             "1,Planet2",
             "2,Planet3",
-            "2,Planet44"
+            "2,Planet4"
     })
     public void getPlanetByIdSuccess(int ownerId, String planetName) {
         Planet toReturn = new Planet();
@@ -54,6 +90,7 @@ public class PlanetServiceTest {
 
     @Test
     @DisplayName("PlanetService::getPlanetByName - Failure")
+    @Order(3)
     public void getPlanetByIdSuccess() {
         int ownerId = 1;
         String planetName = "PlanetDoesNotExist";
@@ -65,6 +102,7 @@ public class PlanetServiceTest {
 
     @ParameterizedTest
     @DisplayName("PlanetService::getPlanetById - Success")
+    @Order(4)
     @CsvSource({
             "1,1",
             "1,2",
@@ -84,6 +122,7 @@ public class PlanetServiceTest {
 
     @Test
     @DisplayName("PlanetService::getPlanetById - Failure")
+    @Order(5)
     public void getPlanetByIdFailure() {
         int ownerId = -1;
         int planetId = -1;
@@ -92,8 +131,78 @@ public class PlanetServiceTest {
         Assertions.assertNull(actual);
     }
 
+    @ParameterizedTest
+    @DisplayName("PlanetService::createPlanet - Success")
+    @Order(6)
+    @CsvSource({
+            "1,Planet1",
+            "1,Planet2",
+            "2,Planet3",
+            "2,Planet4"
+    })
+    public void createPlanetSuccess(int ownerId, String planetName) {
+        Planet toAdd = new Planet();
+        toAdd.setName(planetName);
+        toAdd.setOwnerId(ownerId);
+
+        when(dao.createPlanet(toAdd)).thenReturn(toAdd);
+        Planet actual = planetService.createPlanet(ownerId, toAdd);
+        boolean conditions = toAdd.getOwnerId() == actual.getOwnerId() && toAdd.getName().equals(toAdd.getName());
+        Assertions.assertTrue(conditions);
+    }
+
+    @Test
+    @DisplayName("PlanetService::createPlanet - Empty Name")
+    @Order(7)
+    public void createPlanetEmptyName() {
+        Planet toAdd = new Planet();
+        toAdd.setName("");
+        toAdd.setOwnerId(1);
+
+        Planet actual = planetService.createPlanet(1, toAdd);
+        Planet expected = new Planet();
+
+        boolean condition = actual.getOwnerId() == expected.getOwnerId() && actual.getName() == null;
+
+        Assertions.assertTrue(condition);
+    }
+
+    @Test
+    @DisplayName("PlanetService::createPlanet - Long Name")
+    @Order(8)
+    public void createPlanetLongName() {
+        Planet toAdd = new Planet();
+        toAdd.setName("ThisIsMoreThan30Charactersaaaaa");
+        toAdd.setOwnerId(1);
+
+        Planet actual = planetService.createPlanet(1, toAdd);
+        Planet expected = new Planet();
+
+        boolean condition = actual.getOwnerId() == expected.getOwnerId() && actual.getName() == null;
+
+        Assertions.assertTrue(condition);
+    }
+
+    @Test
+    @DisplayName("PlanetService::createPlanet - Failure")
+    @Order(9)
+    public void createPlanetFailure() {
+        Planet toAdd = new Planet();
+        toAdd.setName("ThisFailsForSomeReason");
+        toAdd.setOwnerId(1);
+
+        when(dao.createPlanet(toAdd)).thenReturn(new Planet());
+        Planet actual = planetService.createPlanet(1, toAdd);
+        Planet expected = new Planet();
+
+        boolean condition = actual.getOwnerId() == expected.getOwnerId() && actual.getName() == null;
+
+        Assertions.assertTrue(condition);
+    }
+
     @Test
     @DisplayName("PlanetService::deletePlanetById - Success")
+    @Order(10)
     public void deletePlanetSuccess() {
         when(dao.deletePlanetById(1, 1)).thenReturn(true);
         Assertions.assertTrue(planetService.deletePlanetById(1, 1));
@@ -101,6 +210,7 @@ public class PlanetServiceTest {
 
     @Test
     @DisplayName("PlanetService::deletePlanetById - Failure")
+    @Order(11)
     public void deletePlanetFailure() {
         when(dao.deletePlanetById(1, -1)).thenReturn(false);
         Assertions.assertFalse(planetService.deletePlanetById(1, -1));
