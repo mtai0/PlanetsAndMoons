d0d60a4548c39ac7fba32eff57e8c19c7b9b9006
realge <q6468860681@gmail.com>
2024-05-28 16:17:03 -0400

    moon dao

--- src/test/java/com/revature/integration/databasedao/MoonDaoTest.java
+++ src/test/java/com/revature/integration/databasedao/MoonDaoTest.java
@@ -1,20 +1,35 @@
 package com.revature.integration.databasedao;
 
-import com.revature.repository.MoonDao;
-import com.revature.utilities.ConnectionUtil;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.revature.models.Moon;
+import com.revature.models.Planet;
+import com.revature.repository.MoonDao;
+import com.revature.repository.PlanetDao;
+import com.revature.repository.UserDao;
+import com.revature.utilities.ConnectionUtil;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MoonDaoTest {
     private MoonDao dao;
-
+    private PlanetDao pdao;
+    private UserDao udao;
     private void resetDatabase(){
         try (Connection connection = ConnectionUtil.createConnection()) {
             PreparedStatement psMoon = connection.prepareStatement("DELETE FROM moons;");
@@ -33,7 +48,200 @@ public class MoonDaoTest {
     @BeforeEach
     public void setupTest(){
         dao = new MoonDao();
-        resetDatabase();
+        pdao = new PlanetDao();
+        
+        
+    }
+
+    @ParameterizedTest
+    @Order(0)
+    @DisplayName("Integration::MoonDao::createMoon - Success")
+    @CsvSource({
+        "Luna, -1 ,t1",
+        "Phobos, -1, t2",
+        "Deimos, -1, t3"
+    })
+    public void createMoon(String name, int planetId, String planetName) {
+       
+    
+          int userId= -1;
+        String username = "user";
+        String password = "pass";
+        //Populate database
+        try (Connection connection = ConnectionUtil.createConnection()) {
+
+            //Add user to DB
+            String sql2 = "insert into users (username, password) values (?, ?)";
+            PreparedStatement ps2 = connection.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS);
+            ps2.setString(1, username);
+            ps2.setString(2, password);
+            int userRowsUpdated = ps2.executeUpdate();
+            if (userRowsUpdated <= 0) Assertions.fail("Failed to set up User");
+            ResultSet rsUsers = ps2.getGeneratedKeys();
+            userId = rsUsers.getInt(1);
+
+            //Add Planet to DB
+            String sql = "insert into planets (name, ownerId) values (?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, planetName);
+            ps.setInt(2, userId);
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            Assertions.fail(" failed to populate database due to a SQLException.");
+        }
+         Planet planet= pdao.getPlanetByName(userId, planetName);
+        Moon moon = new Moon();
+        moon.setName(name);
+        moon.setMyPlanetId(planet.getId());
+
+        Moon actual = dao.createMoon(userId, moon); // Assuming ownerId is 1 for simplicity
+
+        Assertions.assertNotNull(actual);
+       
+        Assertions.assertEquals(name, actual.getName());
+    }
+
+    @ParameterizedTest
+    @Order(1)
+    @DisplayName("Integration::MoonDao::getMoonByName - Success")
+    @CsvSource({
+        "Luna, 1, t1",
+        "Phobos, 2, t2"
+    })
+    public void getMoonByName(String moonName, int ownerId, String planetName) {
+
+
+        int userId= -1;
+        String username = "user";
+        String password = "pass";
+        //Populate database
+        try (Connection connection = ConnectionUtil.createConnection()) {
+
+            //Add user to DB
+            String sql2 = "insert into users (username, password) values (?, ?)";
+            PreparedStatement ps2 = connection.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS);
+            ps2.setString(1, username);
+            ps2.setString(2, password);
+            int userRowsUpdated = ps2.executeUpdate();
+            if (userRowsUpdated <= 0) Assertions.fail("Failed to set up User");
+            ResultSet rsUsers = ps2.getGeneratedKeys();
+            userId = rsUsers.getInt(1);
+
+            //Add Planet to DB
+            String sql = "insert into planets (name, ownerId) values (?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, planetName);
+            ps.setInt(2, userId);
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            Assertions.fail(" failed to populate database due to a SQLException.");
+        }
+         Planet planet= pdao.getPlanetByName(userId, planetName);
+        Moon moon = new Moon();
+        moon.setName(moonName);
+        moon.setMyPlanetId(planet.getId());
+
+        Moon actual = dao.createMoon(userId, moon); // Assuming ownerId is 1 for simplicity
+
+        Assertions.assertNotNull(actual);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(moonName, actual.getName());
+    }
+
+    @ParameterizedTest
+    @Order(2)
+    @DisplayName("Integration::MoonDao::getMoonById - Success")
+    @CsvSource({
+        "1, 1", // Assuming Moon ID 1 is for owner 1
+        "2, 2"  // Assuming Moon ID 2 is for owner 2
+    })
+    public void getMoonById(int moonId, int ownerId) {
+
+
+        Moon actual = dao.getMoonById(ownerId, moonId);
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(moonId, actual.getId());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Integration::MoonDao::getAllMoons - Success")
+    public void getAllMoons() {
+
+        int userId= -1;
+        String username = "user";
+        String password = "pass";
+        //Populate database
+        try (Connection connection = ConnectionUtil.createConnection()) {
+
+            //Add user to DB
+            String sql2 = "insert into users (username, password) values (?, ?)";
+            PreparedStatement ps2 = connection.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS);
+            ps2.setString(1, username);
+            ps2.setString(2, password);
+            int userRowsUpdated = ps2.executeUpdate();
+            if (userRowsUpdated <= 0) Assertions.fail("Failed to set up User");
+            ResultSet rsUsers = ps2.getGeneratedKeys();
+            userId = rsUsers.getInt(1);
+
+            //Add Planet to DB
+            String sql = "insert into planets (name, ownerId) values (?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, "t1");
+            ps.setInt(2, userId);
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            Assertions.fail(" failed to populate database due to a SQLException.");
+        }
+        Planet planet= pdao.getPlanetByName(userId, "t1");
+        Moon moon = new Moon();
+        moon.setName("luna");
+        moon.setMyPlanetId(planet.getId());
+
+        dao.createMoon(userId, moon);
+        List<Moon> moons = dao.getAllMoons(userId);
+        Assertions.assertFalse(moons.isEmpty());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Integration::MoonDao::deleteMoonById - Success")
+    public void deleteMoonById() {
+
+        int userId= -1;
+        String username = "user";
+        String password = "pass";
+        //Populate database
+        try (Connection connection = ConnectionUtil.createConnection()) {
+
+            //Add user to DB
+            String sql2 = "insert into users (username, password) values (?, ?)";
+            PreparedStatement ps2 = connection.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS);
+            ps2.setString(1, username);
+            ps2.setString(2, password);
+            int userRowsUpdated = ps2.executeUpdate();
+            if (userRowsUpdated <= 0) Assertions.fail("Failed to set up User");
+            ResultSet rsUsers = ps2.getGeneratedKeys();
+            userId = rsUsers.getInt(1);
+
+            //Add Planet to DB
+            String sql = "insert into planets (name, ownerId) values (?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, "t1");
+            ps.setInt(2, userId);
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            Assertions.fail(" failed to populate database due to a SQLException.");
+        }
+        Planet planet= pdao.getPlanetByName(userId, "t1");
+        Moon moon = new Moon();
+        moon.setName("luna");
+        moon.setMyPlanetId(planet.getId());
+        dao.createMoon(userId, moon);
+        Moon selected= dao.getMoonByName(userId, "luna");
+        boolean result = dao.deleteMoonById(userId, selected.getId());
+        Assertions.assertTrue(result);
     }
 
     @AfterEach
